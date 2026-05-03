71934cfbe8fd2cfa69f7a0e245ce28351f779f61
Mario Nady <marionadydev@gmail.com>
2026-05-03 20:03:28 +0300

    adding elements to home page

--- Cinema/src/app/pages/home/home.ts
+++ Cinema/src/app/pages/home/home.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { FeatureCard } from '../../components/feature-card/feature-card';
+import { UpcomingCard } from "../../components/upcoming-card/upcoming-card";
+import { MovieCard } from "../../components/movie-card/movie-card";
+import { FeaturedMovie } from "../../components/featured-movie/featured-movie";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FeatureCard, UpcomingCard, MovieCard, FeaturedMovie],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
