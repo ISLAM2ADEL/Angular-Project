b0039be75ecd80cca1e468e9fc8d57e9b3df1470
MariamEssam <romaessam570@gmail.com>
2026-05-06 00:03:09 +0300

    use Global style variables & adding sweetalert2 library

--- Cinema/src/app/pages/signup/signup.ts
+++ Cinema/src/app/pages/signup/signup.ts
@@ -4,12 +4,13 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { minLength } from '@angular/forms/signals';
 import { Router, RouterLink } from '@angular/router';
 import { Auth } from '../../services/auth';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-signup',
   imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './signup.html',
-  styleUrl: './signup.css',
+  styleUrl: '../../SharedAuthStyle/SharedAuth.css',
 })
 export class Signup {
   signupForm: FormGroup;
@@ -35,11 +36,22 @@ private authService = inject(Auth);
       this.authService.register(this.signupForm.value).subscribe({
         next: (response) => {
           console.log('Registration successful:', response);
+          Swal.fire({
+            icon: 'success',
+            title: 'Registration Successful',
+            text: 'Your account has been created successfully! Please sign in to continue.',
+            confirmButtonColor: '#3b1e2a',
+          })
           this.router.navigate(['/sign-in']);
         },
         error: (error) => {
           console.error('Registration failed:', error);
-          alert(error.error.message || 'Error during sign up');
+          Swal.fire({
+            icon: 'error',
+            title: 'Registration Failed',
+            text: error.error.message || 'An error occurred during registration. Please try again.',
+            confirmButtonColor: '#3b1e2a',
+          })
         }
       });
     } else {
